b0d629d8af1e57ee3145de355b5698a6e09d408d
Danesh Paul <dnpaul97@gmail.com>
2021-01-31 22:06:30 +0000

    set debug value

--- django_project/settings.py
+++ django_project/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (os.environ.get('BLOG_APP_SECRET_KEY')  == 'True')
+SECRET_KEY = os.environ.get('BLOG_APP_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('BLOG_DEBUG')
+DEBUG = (os.environ.get('BLOG_DEBUG') == 'True')
 
 ALLOWED_HOSTS = ['dnpaul97-blog.herokuapp.com']
 
